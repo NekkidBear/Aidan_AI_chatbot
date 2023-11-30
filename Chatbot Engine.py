d0957438f73105809c610c0de2f0f9ac91f049dd
NekkidBear <jmking2009@live.com>
2023-11-30 11:37:35 -0600

    added pre-rigged blender model for testing and updated variable names

--- Chatbot Engine.py
+++ Chatbot Engine.py
@@ -3,42 +3,45 @@ import pyttsx3
 from transformers import pipeline
 import subprocess
 
-# Initialize speech recognition engine
-r = sr.Recognizer()
 
-# Initialize text-to-speech engine
-engine = pyttsx3.init()
+def chatbot_engine():
+    # Initialize speech recognition engine
+    r = sr.Recognizer()
 
-# Initialize GPT model
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
+    # Initialize text-to-speech engine
+    engine = pyttsx3.init()
 
-# prompt the user for an audio response
-print("Ask me a question using your microphone")
+    # Initialize GPT model
+    generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 
-# Use default microphone as source
-with sr.Microphone() as source:
-    print("Speak now!")
-    # Adjust for ambient noise
-    r.adjust_for_ambient_noise(source)
-    # Record audio
-    audio = r.listen(source)
+    # prompt the user for an audio response
+    print("Ask me a question using your microphone")
 
-# Convert speech to text
-text = r.recognize_google(audio)
+    # Use default microphone as source
+    with sr.Microphone() as source:
+        print("Speak now!")
+        # Adjust for ambient noise
+        r.adjust_for_ambient_noise(source)
+        # Record audio
+        audio = r.listen(source)
 
-# Generate continuation of text using GPT
-generated_text = generator(text, max_length=100)[0]['generated_text']
+    # Convert speech to text
+    text = r.recognize_google(audio)
 
-# Convert text to speech
-engine.say(generated_text)
-engine.runAndWait()
+    # Generate continuation of text using GPT
+    generated_text = generator(text, max_length=100)[0]['generated_text']
 
-# Call Blender from the command line and pass the script as an argument
-subprocess.call(['blender', '-b', 'C:\\Users\\jmkin\\OneDrive\\Documents\\blender models\\Igor\\.blend', '-P', 'Chatbot Engine.py'])
+    # Convert text to speech
+    engine.say(generated_text)
+    engine.runAndWait()
 
-# Generate mouth-shape keyframes using Blender Rhubarb plugin
-subprocess.call(['rhubarb-lipsync', '-i', 'audio.wav', '-o', 'mouth-shapes.txt'])
+    # Call Blender from the command line and pass the script as an argument
+    subprocess.call(['blender', '-b', 'C:\\Users\\jmkin\\OneDrive\\Documents\\blender models\\Igor\\.blend', '-P',
+                     'Chatbot Engine.py'])
 
-# Use the generated mouth-shape keyframes to animate the mouth of your 3D character in Blender
-print(f"Recognized text: {text}")
-print(f"Generated text: {generated_text}")
+    # Generate mouth-shape keyframes using Blender Rhubarb plugin
+    subprocess.call(['rhubarb-lipsync', '-i', 'audio.wav', '-o', 'mouth-shapes.txt'])
+
+    # Use the generated mouth-shape keyframes to animate the mouth of your 3D character in Blender
+    print(f"Recognized text: {text}")
+    print(f"Generated text: {generated_text}")
--- display_screen.py
+++ display_screen.py
@@ -1,25 +1,24 @@
 import tkinter as tk
 
-# Create the main window
-root = tk.Tk()
 
-# Set the window title
-root.title("Display Screen")
+def display_screen():
+    # Create the main window
+    root = tk.Tk()
 
-# Set the window size
-root.geometry("800x600")
+    # Set the window title
+    root.title("Chatbot")
 
-# Create the left pane for the character animation
-left_pane = tk.Frame(root, width=400, height=600, bg="white")
-left_pane.pack(side="left", fill="both")
+    # Set the window size
+    root.geometry("800x600")
 
-# Create the middle pane for the chat text
-middle_pane = tk.Frame(root, width=400, height=300, bg="white")
-middle_pane.pack(side="top", fill="both")
+    # Create the left pane for the character animation
+    left_pane = tk.Frame(root, width=400, height=600, bg="white")
+    left_pane.pack(side="left", fill="both")
 
-# Create the right pane for links and other returned information
-right_pane = tk.Frame(root, width=400, height=300, bg="white")
-right_pane.pack(side="bottom", fill="both")
+    # Create the middle pane for the chat text
+    middle_pane = tk.Frame(root, width=400, height=300, bg="white")
+    middle_pane.pack(side="top", fill="both")
 
-# Start the main event loop
-root.mainloop()
+    # Create the right pane for links and other returned information
+    right_pane = tk.Frame(root, width=400, height=300, bg="white")
+    right_pane.pack(side="bottom", fill="both")
