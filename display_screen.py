f3a5044d02c67334c09d06f0fb8c37151fdefc22
NekkidBear <jmking2009@live.com>
2023-12-02 17:27:03 -0600

    add default image to display_screen.py and main.py

--- display_screen.py
+++ display_screen.py
@@ -1,21 +1,23 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+
 class DisplayScreen:
     def __init__(self, root):
+        self.image_label = None
+        self.photo_img = None
         self.root = root
-        self.image_label = tk.Label(root)
-        self.image_label.pack()
 
-    def load_image(self, image_path):
+    def load_image(self, image_path, frame):
         img = Image.open(image_path)
         img_width, img_height = img.size
-        max_width, max_height = self.root.winfo_screenwidth(), self.root.winfo_screenheight()
-        scale = min(max_width/img_width, max_height/img_height)
-        new_width, new_height = int(img_width*scale), int(img_height*scale)
-        img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        max_width, max_height = frame.winfo_screenwidth(), frame.winfo_screenheight()
+        scale = min(max_width / img_width, max_height / img_height)
+        new_width, new_height = int(img_width * scale), int(img_height * scale)
+        img = img.resize((new_width, new_height), Image.LANCZOS)
         self.photo_img = ImageTk.PhotoImage(img)
-        self.image_label.config(image=self.photo_img)
+        self.image_label = tk.Label(frame, image=self.photo_img)
+        self.image_label.grid(sticky='nsew')
 
     def start(self):
         # Start the tkinter display
--- main.py
+++ main.py
@@ -4,16 +4,31 @@ from display_screen import DisplayScreen
 
 
 class MainApp:
-    def __init__(self, root):
-        self.root = root
-        # self.chatbot_engine = chatbot_engine()
+    def __init__(self, root_window):
+        self.root = root_window
         self.display_screen = DisplayScreen(root)
+
+        # Create frames for the image, web browser, and chat transcript
+        self.image_frame = tk.Frame(root)
+        self.web_frame = tk.Frame(root)
+        self.chat_frame = tk.Frame(root)
+
+        # Position the frames in the grid
+        self.image_frame.grid(row=0, column=0, rowspan=4, sticky='nsew')
+        self.web_frame.grid(row=0, column=1, columnspan=3, rowspan=3, sticky='nsew')
+        self.chat_frame.grid(row=3, column=1, columnspan=3, sticky='nsew')
+
+        # Configure the grid to distribute space between frames
+        for i in range(4):
+            root.grid_rowconfigure(i, weight=1)
+            if i > 0:
+                root.grid_columnconfigure(i, weight=1)
 
     def start(self):
         # Start the chatbot engine and tkinter display
         # self.chatbot_engine.start()
         self.display_screen.start()
-        self.display_screen.load_image("assets\\graphics\\Aidan Interactive AI avatar.png")
+        self.display_screen.load_image("assets\\graphics\\Aidan Interactive AI avatar.png", self.image_frame)
 
     def update(self):
         # Update the chatbot engine and tkinter display
