2d09e9054c82892188f31425d399430344413fdc
NekkidBear <jmking2009@live.com>
2023-12-02 19:31:38 -0600

    resizing window panes

--- main.py
+++ main.py
@@ -10,38 +10,29 @@ class MainApp:
         self.display_screen = DisplayScreen(root)
 
         # Create frames for the image, web browser, and chat transcript with MS Teams dark mode colors
-        self.image_frame = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
         self.web_frame = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
         self.chat_frame = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-
-        # create a label widget with white text inside the image frame
-        character_label = tk.Label(self.image_frame, text="Character Image", fg="white")
-        character_label.grid()
-        web_label = tk.Label(self.web_frame, text="Web browser", fg="white")
-        web_label.grid()
-        chat_label = tk.Label(self.chat_frame, text="Text Transcript", fg="White")
-        chat_label.grid()
+        self.image_frame1 = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
+        self.image_frame2 = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
 
         # Position the frames in the grid
-        self.web_frame.grid(row=0, column=0, columnspan=2, rowspan=3, sticky='nsew')
-        self.chat_frame.grid(row=3, column=0, columnspan=2, sticky='nsew')
-        self.image_frame.grid(row=0, column=2, rowspan=4, columnspan=2, sticky='nsew')
+        self.web_frame.grid(row=0, column=0, sticky='nsew')
+        self.chat_frame.grid(row=1, column=0, sticky='nsew')
+        self.image_frame1.grid(row=0, column=1, sticky='nsew')
+        self.image_frame2.grid(row=1, column=1, sticky='nsew')
 
         # Configure the grid to distribute space between frames
-        for i in range(4):
+        for i in range(2):
             root.grid_rowconfigure(i, weight=1)
-        for i in range(3):
-            if i < 2:
-                root.grid_columnconfigure(i, weight=1)
-            else:
-                root.grid_columnconfigure(i, weight=2)
+        root.grid_columnconfigure(0, weight=2)  # web and chat frames take up 2/3 of the width
+        root.grid_columnconfigure(1, weight=1)  # image frames take up 1/3 of the width
 
     def start(self):
         # Start the chatbot engine and tkinter display
         # self.chatbot_engine.start()
         self.display_screen.start()
         self.root.update()  # update the tkinter window
-        self.display_screen.load_image("assets\\graphics\\Aidan Interactive AI avatar.png", self.image_frame)
+        self.display_screen.load_image("assets\\graphics\\Aidan Interactive AI avatar.png", self.image_frame1)
 
     def update(self):
         # Update the chatbot engine and tkinter display
