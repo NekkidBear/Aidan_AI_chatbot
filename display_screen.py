884daf24961e35bd368417972eb35d86183dc163
NekkidBear <jmking2009@live.com>
2023-12-04 12:47:39 -0600

    rewriting display_screen.py to use pyqt5 instead of tkinter

--- display_screen.py
+++ display_screen.py
@@ -1,55 +1,10 @@
-import tkinter as tk
-from PIL import Image, ImageTk
+# display_screen.py
+from PyQt5.QtWidgets import QTextEdit
 
 
-class DisplayScreen:
-    def __init__(self, root):
-        self.root = root
-        self.paned_window = tk.PanedWindow(root, orient=tk.HORIZONTAL)
-        self.paned_window.grid(sticky='nsew')
+class DisplayScreen(QTextEdit):
+    def __init__(self, parent=None):
+        super(DisplayScreen, self).__init__(parent)
 
-        # Create the browser frame (3/4 of the window)
-        self.browser_frame = tk.Frame(self.paned_window, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.paned_window.add(self.browser_frame)
-
-        # Create the side panel frame (1/4 of the window)
-        self.side_panel = tk.PanedWindow(self.paned_window, orient=tk.VERTICAL)
-        self.paned_window.add(self.side_panel)
-
-        # Create the top and bottom frames inside the side panel
-        self.top_frame = tk.Frame(self.side_panel, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.bottom_frame = tk.Frame(self.side_panel, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.side_panel.add(self.top_frame)
-        self.side_panel.add(self.bottom_frame)
-
-        self.image_label = None
-        self.text_widget = None
-        self.photo_img = None
-
-    def load_image(self, image_path, frame):
-        frame.update_idletasks()  # Ensure that the frame has been displayed
-        img = Image.open(image_path)
-        img_width, img_height = img.size
-        max_width, max_height = frame.winfo_width(), frame.winfo_height()
-        scale = min(max_width / img_width, max_height / img_height)
-        new_width, new_height = int(img_width * scale), int(img_height * scale)
-        img = img.resize((new_width, new_height), Image.LANCZOS)
-        self.photo_img = ImageTk.PhotoImage(img)
-        self.image_label = tk.Label(frame, image=self.photo_img)
-        self.image_label.grid(sticky='nsew')
-
-    def display_text(self, text, frame):
-        if self.text_widget is None:
-            self.text_widget = tk.Text(frame)
-            self.text_widget.grid(sticky='nsew')
-        self.text_widget.insert(tk.END, text)
-
-    def start(self):
-        # Start the tkinter display
-        # Set the initial size of the panes
-        self.browser_frame.config(width=self.root.winfo_width() * 3 // 4)
-        self.side_panel.config(width=self.root.winfo_width() // 4)
-
-    def update(self):
-        # Update the tkinter display
-        pass
+    def display_text(self, text):
+        self.append(text)
--- main.py
+++ main.py
@@ -1,55 +1,35 @@
-import tkinter as tk
+# main.py
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from chatbot_engine import ChatbotEngine
 from display_screen import DisplayScreen
 
 
-class MainApp:
-    def __init__(self, root_window):
-        self.display_screen = DisplayScreen(root)
+class MainApp(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
         self.chatbot_engine = ChatbotEngine()
-        self.root = root_window
-        self.root.geometry('800x600')
+        self.display_screen = DisplayScreen()
 
-        # Create frames for the image, web browser, and chat transcript with MS Teams dark mode colors
-        self.web_frame = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.chat_frame = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.image_frame1 = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
-        self.image_frame2 = tk.Frame(root, bg="#201f1f", highlightbackground="#464775", highlightthickness=1)
+        # Create stop button
+        self.stop_button = QPushButton("Stop")
+        self.stop_button.clicked.connect(self.chatbot_engine.stop)
 
-        # Position the frames in the grid
-        self.web_frame.grid(row=0, column=0, sticky='nsew')
-        self.chat_frame.grid(row=1, column=0, sticky='nsew')
-        self.image_frame1.grid(row=0, column=1, sticky='nsew')
-        self.image_frame2.grid(row=1, column=1, sticky='nsew')
+        # Connect the chatbot engine's text_generated signal to a slot that updates the display screen
+        self.chatbot_engine.text_generated.connect(self.display_screen.display_text)
 
-        # Configure the grid to distribute space between frames
-        for i in range(2):
-            root.grid_rowconfigure(i, weight=1)
-        root.grid_columnconfigure(0, weight=2)  # web and chat frames take up 2/3 of the width
-        root.grid_columnconfigure(1, weight=1)  # image frames take up 1/3 of the width
+        # Create a layout and add the widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.display_screen)
+        layout.addWidget(self.stop_button)
 
-    def start(self):
-        # Start the chatbot engine and tkinter display
-        self.display_screen.start()
-        self.root.update()  # update the tkinter window
-        self.display_screen.load_image("assets\\graphics\\Aidan Interactive AI avatar.png", self.image_frame1)
-        # Start the chatbot engine and tkinter display
-        recognized_text, generated_text = self.chatbot_engine.start()
-        self.display_screen.display_text(f"You: {recognized_text}", self.display_screen.bottom_frame)
-        self.display_screen.display_text(f"Aidan: {generated_text}", self.display_screen.bottom_frame)
-
-    def update(self):
-        # Update the chatbot engine and tkinter display
-        recognized_text, generated_text = self.chatbot_engine.update()
-        self.display_screen.display_text(f"You: {recognized_text}", self.display_screen.bottom_frame)
-        self.display_screen.display_text(f"Aidan: {generated_text}", self.display_screen.bottom_frame)
-
-        # Schedule the next update
-        self.root.after(1000, self.update)  # Update every second
+        # Create a central widget, set the layout, and set the central widget
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = MainApp(root)
-    app.start()
-    root.mainloop()
+    app = QApplication([])
+    main_app = MainApp()
+    main_app.show()
+    app.exec_()
